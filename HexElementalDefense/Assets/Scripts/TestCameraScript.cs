b888368de60344c8f1df7dd863547c10a2d8b4bc
Jiří Bobrek <bobrekjiri@gmail.com>
2016-10-02 11:25:44 +0200

    Camera positioning test script added

---
+++ HexElementalDefense/Assets/Scripts/TestCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TestCameraScript : MonoBehaviour
+    {
+
+        float horizontalSpeed = 4.0f;
+        float verticalSpeed = 4.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                var h = horizontalSpeed * Input.GetAxis("Mouse Y");
+                var v = verticalSpeed * Input.GetAxis("Mouse X");
+                transform.Rotate(h, v, 0);
+            }
+
+        }
+    }
+}
