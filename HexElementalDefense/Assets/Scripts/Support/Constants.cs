cd0d1fbec1a961c5f5014485be54242e04dfe150
Setalsikaret <honziksima@seznam.cz>
2016-10-02 14:31:14 +0200

    Map  generation change
    Map script now depends on MapBuilder
    Added Constants with MapBuilder(s)

---
+++ HexElementalDefense/Assets/Scripts/Support/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.MapCreation;
+using Assets.Scripts.MapObjects;
+
+namespace Assets.Scripts.Support
+{
+    /// <summary>
+    /// Trida poskytujici globalni pomocne konstanty
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Seznam dostupnych MapBuilderu
+        /// </summary>
+        public static IMapBuilder[] AviableMapBuilders = new IMapBuilder[]
+        {
+            new TestMapBuilder(),
+        };
+    }
+}
